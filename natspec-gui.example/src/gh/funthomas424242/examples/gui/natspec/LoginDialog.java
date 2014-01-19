6418ff8582089ff3a836ec8cf53b03dd3454c838
Thomas Schubert <funthomas424242@gmail.com>
2014-01-19 21:31:28 +0100

    stable project structure
    Signed-off-by: Thomas Schubert <funthomas424242@gmail.com>

--- natspec-gui.example/src/gh/funthomas424242/examples/gui/natspec/LoginDialog.java
+++ natspec-gui.example/src/gh/funthomas424242/examples/gui/natspec/LoginDialog.java
@@ -13,6 +13,16 @@ public class LoginDialog {
 		// The code in this method is generated from: /natspec-gui.example/src/gh/funthomas424242/examples/gui/natspec/LoginDialog.natspec
 		// Never change this method or any contents of this file, all local changes will be overwritten.
 		
+		// Beschreibung des Dialoges blah mit:
+		javax.swing.JFrame jFrame_blah = swingSupport.createDialog("blah");
+		// 300 200
+		swingSupport.setSize(300, 200, jFrame_blah);
+		// Hallo Welt mit Swing
+		swingSupport.setTitel(java.util.Arrays.asList(new java.lang.String[] {"Hallo", "Welt", "mit", "Swing"}), jFrame_blah);
+		// ja
+		swingSupport.setVisibility("ja", jFrame_blah);
+		// Schliesse diesen Dialog beim Beenden der Anwendung.
+		swingSupport.setExitOnClose(jFrame_blah);
 		
 		
 		
