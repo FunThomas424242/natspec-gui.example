3eb5bf6c0c1895dc53043ba9fcf863950c780d28
Thomas Schubert <funthomas424242@gmail.com>
2014-01-26 21:55:17 +0100

    Flow Formatting fixed
    Signed-off-by: Thomas Schubert <funthomas424242@gmail.com>

--- natspec-swingapp.example/src/main/java/gh/funthomas424242/examples/flow/BeendenFlow.java
+++ natspec-swingapp.example/src/main/java/gh/funthomas424242/examples/flow/BeendenFlow.java
@@ -6,17 +6,43 @@ import gh.funthomas424242.examples.lib.BusinessModel;
 
 public class BeendenFlow extends SubFlow implements Runnable {
 
-    final protected FlowSupport flowSupport;
+    final private FlowSupport flowSupport;
 
+    /**
+     * Diese Methode gibt die unterliegende Supoort Klasse zurÃ¼ck
+     * 
+     * @return flowSupport
+     */
+    public FlowSupport getFlowSupport() {
+        return flowSupport;
+    }
+
+    /**
+     * Konstruktor eines Subflow.
+     * 
+     * @param model
+     *            globales Model der Anwendung.
+     * @param parentFlow
+     *            Flow zu dem der aktuelle Flow ein Teilflow ist.
+     */
     public BeendenFlow(final BusinessModel model, final Flow parentFlow) {
         super(model, parentFlow);
         flowSupport = new FlowSupport(model, this);
     }
 
+    /**
+     * Konstruktur fÃ¼r einen Top Level Flow.
+     * 
+     * @param model
+     *            globales Model der Anwendung.
+     */
     public BeendenFlow(final BusinessModel model) {
         this(model, null);
     }
 
+    /**
+     * Methode zur AusfÃ¼hrung des Flows.
+     */
     @Override
     public void run() {
         // generated code will be inserted here
--- natspec-swingapp.example/src/main/java/gh/funthomas424242/examples/flow/_NatSpecTemplate.java
+++ natspec-swingapp.example/src/main/java/gh/funthomas424242/examples/flow/_NatSpecTemplate.java
@@ -6,17 +6,43 @@ import gh.funthomas424242.examples.lib.BusinessModel;
 
 public class _NatSpecTemplate extends SubFlow implements Runnable {
 
-    final protected FlowSupport flowSupport;
+    final private FlowSupport flowSupport;
 
+    /**
+     * Diese Methode gibt die unterliegende Supoort Klasse zurück
+     * 
+     * @return flowSupport
+     */
+    public FlowSupport getFlowSupport() {
+        return flowSupport;
+    }
+
+    /**
+     * Konstruktor eines Subflow.
+     * 
+     * @param model
+     *            globales Model der Anwendung.
+     * @param parentFlow
+     *            Flow zu dem der aktuelle Flow ein Teilflow ist.
+     */
     public _NatSpecTemplate(final BusinessModel model, final Flow parentFlow) {
         super(model, parentFlow);
         flowSupport = new FlowSupport(model, this);
     }
 
+    /**
+     * Konstruktur für einen Top Level Flow.
+     * 
+     * @param model
+     *            globales Model der Anwendung.
+     */
     public _NatSpecTemplate(final BusinessModel model) {
         this(model, null);
     }
 
+    /**
+     * Methode zur Ausführung des Flows.
+     */
     @Override
     public void run() {
         // generated code will be inserted here
