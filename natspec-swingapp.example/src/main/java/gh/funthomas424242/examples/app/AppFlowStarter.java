93574cfafb15acd11c44bf4dcbe60564840ad965
Thomas Schubert <funthomas424242@gmail.com>
2014-01-26 21:41:34 +0100

    formatter added
    Signed-off-by: Thomas Schubert <funthomas424242@gmail.com>

--- natspec-swingapp.example/src/main/java/gh/funthomas424242/examples/app/AppFlowStarter.java
+++ natspec-swingapp.example/src/main/java/gh/funthomas424242/examples/app/AppFlowStarter.java
@@ -5,18 +5,18 @@ import gh.funthomas424242.examples.lib.BusinessModel;
 
 public class AppFlowStarter {
 
-	public void execute() {
-		final BusinessModel model = new BusinessModel();
-		final MainFlow flow = new MainFlow(model);
-		flow.run();
-		// final Thread thread = new Thread(mainFlow);
-		// thread.start();
-	}
+    public void execute() {
+        final BusinessModel model = new BusinessModel();
+        final MainFlow flow = new MainFlow(model);
+        flow.run();
+        // final Thread thread = new Thread(mainFlow);
+        // thread.start();
+    }
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		final AppFlowStarter mainFlow = new AppFlowStarter();
-		mainFlow.execute();
+        final AppFlowStarter mainFlow = new AppFlowStarter();
+        mainFlow.execute();
 
-	}
+    }
 }
